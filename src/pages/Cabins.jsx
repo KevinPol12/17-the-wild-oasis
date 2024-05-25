b90637b9a38bddef80522abb79f702b356a93819
KevinPol12 <kevinpolanco12@gmail.com>
2024-05-25 12:25:19 -0600

    Created a quick supabase database to host our app data using RLS to prevent unauthorized actions even when having the api key, and then using the api to start querying our quick database from our from our front-end application without having to setup a whole back-end REST api

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
+  const [cabins, setCabins] = useState([]);
+
+  useEffect(function () {
+    async function execute() {
+      const cabins = await getCabins();
+
+      console.log(cabins);
+      setCabins(cabins);
+    }
+    execute();
+  }, []);
+
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+      </Row>
+      <ul>
+        List of Cabins
+        {cabins.map((cabin) => (
+          <li key={cabin.id}>
+            <h2>{cabin.name}</h2>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <p>Price: {cabin.regularPrice}</p>
+              <p>Discount: {cabin.discount}</p>
+              <p>Description: {cabin.description}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://qdocrvjvfwizasblxqow.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkb2Nydmp2ZndpemFzYmx4cW93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY2NTM0OTksImV4cCI6MjAzMjIyOTQ5OX0.THhhtaF8WoyWel9zHGdZG6Rk_8zKU-K9BSk5ajTG1dE";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
