ebb89661ed788b5b71da780a7db2ade974172184
KevinPol12 <kevinpolanco12@gmail.com>
2024-05-27 12:28:16 -0600

    Finally querying our database through REACT query's useQuery() to manage our remote state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
@@ -10,28 +13,36 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      //staleTime allows us to determine for how long reactQuery should consider this state valid, so when it expires it re-fetches the data.
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
   return (
-    <div>
-      <>
-        <GlobalStyles />
-        <BrowserRouter>
-          <Routes>
-            <Route element={<AppLayout />}>
-              <Route index element={<Navigate replace to="dashboard" />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/bookings" element={<Bookings />} />
-              <Route path="/cabins" element={<Cabins />} />
-              <Route path="/users" element={<Users />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/account" element={<Account />} />
-            </Route>
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/bookings" element={<Bookings />} />
+            <Route path="/cabins" element={<Cabins />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/account" element={<Account />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,39 +1,17 @@
-import { useEffect, useState } from "react";
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
-  const [cabins, setCabins] = useState([]);
-
-  useEffect(function () {
-    async function execute() {
-      const cabins = await getCabins();
-
-      console.log(cabins);
-      setCabins(cabins);
-    }
-    execute();
-  }, []);
-
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
+        <p>Filter/sort</p>
       </Row>
-      <ul>
-        List of Cabins
-        {cabins.map((cabin) => (
-          <li key={cabin.id}>
-            <h2>{cabin.name}</h2>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <p>Price: {cabin.regularPrice}</p>
-              <p>Discount: {cabin.discount}</p>
-              <p>Description: {cabin.description}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <Row>
+        <CabinTable />
+      </Row>
     </>
   );
 }
