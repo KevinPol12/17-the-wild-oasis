7db32acd8a897a102d1408271af9075b1dc115aa
KevinPol12 <kevinpolanco12@gmail.com>
2024-05-28 09:20:15 -0600

    Wiring up our react-hook-form

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,5 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
+import { useQuery } from "@tanstack/react-query";
 import { getCabins } from "../../services/apiCabins";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
